cc02eaee2bbb0cdb09f832c8f3ca0f4c0522e3e1
LumenPlexus <2725754457@qq.com>
2026-08-25 05:48:06 +0000

    初版发布：平万象大学生全能成长工作台 v2

---
+++ pwa/sw.js
@@ -0,0 +1,28 @@
+/* 平·万象 Service Worker：让网站可安装为桌面 App、并支持断网也能打开 */
+const CACHE = 'pingwanxiang-v1';
+const CORE = ['./', './index.html', './pwa/manifest.json', './pwa/icon-192.png', './pwa/icon-512.png'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(CORE)).catch(() => {}));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keys) =>
+    Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
+  if (url.origin !== self.location.origin) return; // 不缓存跨域资源
+  e.respondWith(
+    fetch(req).then((res) => {
+      const copy = res.clone();
+      caches.open(CACHE).then((c) => c.put(req, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match(req).then((hit) => hit || caches.match('./index.html')))
+  );
+});
