a2230be7b793819ec9812d84492fe58b1c4ba635
LumenPlexus <LumenPlexus@users.noreply.github.com>
2026-08-31 06:48:48 +0000

    v2026.08.31.3: 顶栏恢复经典四键模板(换灵感/导览/夜间/更多) · 更多菜单按图分组补全 · 全局搜索移入菜单顶 · 圆形按钮视觉统一

--- sw.js
+++ sw.js
@@ -5,11 +5,11 @@
  *  - 用户所有记录数据一律保存在本机浏览器 localStorage，绝不上传任何服务器、
  *    也绝不经由本 Service Worker 发送或中转任何数据。
  *  - 升级版号后，浏览器会自动拉取新版缓存并刷新一次，此后再次打开即为最新内容。
- * 版本：2026.08.31.2
+ * 版本：2026.08.31.3
  */
 var CACHE_DATE = '2026.08.31';
 
-var CACHE_NAME = 'qi-xia-' + CACHE_DATE.replace(/\./g, '') + '-31b';
+var CACHE_NAME = 'qi-xia-' + CACHE_DATE.replace(/\./g, '') + '-3b';
 var PRECACHE = [
   './',
   './index.html',
