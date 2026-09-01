8709386bedfe4b56e7646a04b2b37b2dbd3d257f
Qixia Deploy <deploy@qixia.local>
2026-09-01 07:14:47 +0000

    栖匣站点更新 v2026.08.31.17：更多菜单点击加固+图标加大精美 2026-09-01 07:14

--- js/upgrade.js
+++ js/upgrade.js
@@ -8,7 +8,7 @@
 
   var NS = window.QixiaUpgrade = {};
   var STORE_KEY = 'pp_cu_config_v1';
-  var DATA_VERSION = '2026.08.31.17';
+  var DATA_VERSION = '2026.08.31.18';
 
   /* ============================================================
      1. 通用工具
