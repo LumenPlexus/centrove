47cece4428588acda6e1975a72bc7f27b7215197
deploy <deploy@local>
2026-09-02 17:18:19 +0000

    2026.09.02.9: 分享/导出链接统一改为网站地址 https://lumenplexus.github.io/centrove/，不再带 trae-preview 本地地址

--- js/upgrade.js
+++ js/upgrade.js
@@ -8,7 +8,7 @@
 
   var NS = window.QixiaUpgrade = {};
   var STORE_KEY = 'pp_cu_config_v1';
-  var DATA_VERSION = '2026.09.02.8';
+  var DATA_VERSION = '2026.09.02.9';
 
   /* ============================================================
      1. 通用工具
