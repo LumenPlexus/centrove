b290d465a3268a54f43261425ed790a151d0fd0e
LumenPlexus <2725754457@qq.com>
2026-09-04 04:08:26 +0800

    chore: 桌面图标换代摆脱栖字占位，新文件名图标+全出血奶油底，统一版本2026.09.09.01

--- js/upgrade.js
+++ js/upgrade.js
@@ -8,7 +8,7 @@
 
   var NS = window.QixiaUpgrade = {};
   var STORE_KEY = 'pp_cu_config_v1';
-  var DATA_VERSION = '2026.09.06.01';
+  var DATA_VERSION = '2026.09.09.01';
 
   /* ============================================================
      1. 通用工具
