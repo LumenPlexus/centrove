8e078ebbdf4fbb804540e24b646b50c3ca5f9550
centrove-deploy <deploy@centrove.local>
2026-09-04 18:47:58 +0000

    图标彻底改名:全新文件名favicon-chest20260910-*消除桌面旧图标缓存;移除巨型data-URI favicon;刷新share/og图;版本2026.09.06.07

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    - HTML 导航采用网络优先（保证每次拿到最新页面），静态资源采用 stale-while-revalidate
      （离线秒开、在线自动后台刷新）。
    安全说明：本 SW 只缓存本站静态资源，绝不读写、上传任何 localStorage 用户数据。 */
-var VERSION = '2026.09.06.06';
+var VERSION = '2026.09.06.07';
 var PRE = 'centrove-pre-' + VERSION;
 var RUN = 'centrove-run-' + VERSION;
 
@@ -17,6 +17,14 @@ var PRECACHE_URLS = [
   './js/upgrade.js',
   './pwa/manifest.json',
   './pwa/version.txt',
+  './favicon-chest20260910-16.png',
+  './favicon-chest20260910-32.png',
+  './favicon-chest20260910-96.png',
+  './favicon-chest20260910-192.png',
+  './favicon-chest20260910-180.png',
+  './pwa/icon-chest20260910-192.png',
+  './pwa/icon-chest20260910-512.png',
+  './pwa/share-chest20260910.png',
   './favicon.ico',
   './favicon-16.png',
   './favicon-32.png',
