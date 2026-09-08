ae8fa05352c0e253b8301714728db933a089cee3
qixia <qixia@users.noreply.github.com>
2026-09-08 11:26:56 +0000

    统一产品logo为chest20260907，清理sw缓存残留的异变图标

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    - HTML 导航采用网络优先（保证每次拿到最新页面），静态资源采用 stale-while-revalidate
      （离线秒开、在线自动后台刷新）。
    安全说明：本 SW 只缓存本站静态资源，绝不读写、上传任何 localStorage 用户数据。 */
-var VERSION = '2026.09.08.v6';
+var VERSION = '2026.09.08.v7';
 var PRE = 'centrove-pre-' + VERSION;
 var RUN = 'centrove-run-' + VERSION;
 
@@ -27,23 +27,7 @@ var PRECACHE_URLS = [
   './pwa/icon-chest20260907-192.png',
   './pwa/icon-chest20260907-512.png',
   './pwa/logo-chest20260907.png',
-  './pwa/share-chest20260907.png',
-  './favicon.ico',
-  './favicon-16.png',
-  './favicon-32.png',
-  './favicon-48.png',
-  './favicon-96.png',
-  './favicon.svg',
-  './pwa/apple-touch-icon.png',
-  './pwa/logo.png',
-  './pwa/icon-20260910-192.png',
-  './pwa/icon-20260910-512.png',
-  './pwa/logo.png',
-  './pwa/maskable-192.png',
-  './pwa/maskable-512.png',
-  './pwa/maskable-20260910-192.png',
-  './pwa/maskable-20260910-512.png',
-  './pwa/share-512.png'
+  './pwa/share-chest20260907.png'
 ];
 
 self.addEventListener('install', function (e) {
